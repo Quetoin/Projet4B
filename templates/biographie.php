d56fe2e3c7762435b83b66fe4424f0da48db8689
Quentin Armanville <armanvillequentin@gmail.com>
2020-05-01 10:29:44 +0200

    again, adding some style

--- templates/biographie.php
+++ templates/biographie.php
@@ -4,7 +4,20 @@
 <h2 class="titresWhite">Ma biographie</h2>
 
   <div id="mainDivBiographie" class="mainDiv">
-    <img src="">
-    <p>Bonjour et bienvenue sur l'explication de ma vie. Pourquoi je suis devenu ce que je suis aujourd'hui. Qu'est-ce qui a fait que je suis devenu cet écrivain génial et modeste.
-    </p>
+    <img src="img/portrait.jpg" id="portrait" alt="portrait Jean Forteroche">
+    <p>Bonjour et bienvenue sur l'explication de ma vie, pourquoi je suis devenu ce que je suis aujourd'hui, qu'est-ce qui a fait que je suis devenu cet écrivain.<br></p>
+
+   	<p>Je suis Jean Forteroche, né dans un petit village d'ariège perdu au fond de la montagne, Irazein. C'est cette enfance dans la montagne qui m'a donné envie de voyager, d'être au plus proche de la nature et surtout de la montagne.<br></p>
+
+    <p>J'ai donc navigué entre la ville et la montagne durant toute ma jeunesse, jusqu'à mes 25 ans ou j'ai décidé de tenter ma chance en ville, dans l'espoir de pouvoir revenir un jour dans ma montagne !<br></p>
+
+    <p>J'ai commencé, comme tout écrivain, par écrire des nouvelles, de la science-fiction, des biographies, du roman, des livres de jeunesse ...<br></p>
+
+    <p>Un beau jour, j'ai eu l'idée d'écrire des aventures qui me parlait, des aventures humaines, pleines de sens. <br>
+    	J'ai donc commencé avec <i>"Le voyage d'une vie"</i>, qui a eu le succès qu'on lui connaît.<br></p>
+
+    <p>Aujourd'hui, je vous présente donc <i>"Un billet simple pour l'Alaska"</i>, dernier roman d'aventure. <br></p>
+
+    <p>J'espère qu'il vous plairat !</p>
+
   </div>
